72840b58ce8d29e7414e1877a6142f6b9285530d
Mónica Polanco <mpolan16@g.holycross.edu>
2020-08-03 16:25:29 -0400

    worked on footer code

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,25 +1,22 @@
 import React from 'react';
-
-
+import { Col, Row, Container } from "../Grid";
 
 function Footer() {
-    return (
-        <footer className="footer">
-        <div className="container">
-        <div className="row">
-          <div className="col l4 offset-l2 s12" id="footerIcons">
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col className="col l4 offset-l2 s12" id="footerIcons">
             <a className="right black-text text-lighten-3" href="https://www.linkedin.com/in/monicapolancofabian/"><i className="fab fa-linkedin"></i></a>
             <a className="right black-text text-lighten-3" href="https://github.com/Mpolan16"> <i className="fab fa-github"></i></a>
             <a className="right black-text text-lighten-3" href="mailto:monicapolancofabian15@gmail.com"><i className="far fa-envelope"></i></a>
-          </div>
+          </Col>
+        </Row>
+        <div className="footer-copyright">
+          <div className="container center black-text text-lighten-3">© 2020 Copyright</div>
         </div>
-      </div>
-      <div className="footer-copyright">
-        <div className="container center black-text text-lighten-3">
-        © 2020 Copyright
-        </div>
-      </div>
-        </footer>
+      </Container>
+    </footer>
     );
 }
 
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Col, Row, Container } from "../Grid";
 
-function Header(props) {
+function Header() {
     return (
         <Container fluid>
         <Row>
           <Col size="sm-12">
+
           </Col>
         </Row>
       </Container>
