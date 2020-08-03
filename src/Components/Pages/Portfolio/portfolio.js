c5ff56247ad062a3f07355395bd09822e5520a8b
Mónica Polanco <mpolan16@g.holycross.edu>
2020-08-03 11:21:57 -0400

    added the portfolio.js file and imported all essential files

---
+++ src/Components/Pages/Portfolio/portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../Header';
+import Projects from '../../Projects';
+import '../../../Style';
+
+function Portfolio() {
+    return (
+        <div>
+            <Header value='Portfolio' />
+            <Projects />
+        </div>
+    );
+}
+
+export default Portfolio;
