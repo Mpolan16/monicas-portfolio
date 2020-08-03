585b42ead6b2ba9e47044c0d761070f74087dc4c
Mónica Polanco <mpolan16@g.holycross.edu>
2020-08-03 11:18:53 -0400

    set up initial about page

---
+++ src/Components/Pages/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../Header';
+import AboutMe from '../../AboutMe';
+import '../../../Style';
+
+function About() {
+    return (
+        <div>
+            <Header value='About Me' />
+            <AboutMe />
+        </div>
+    );
+}
+
+export default About;
