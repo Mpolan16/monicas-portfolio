f10be33f5d6ce2de8df27e5d6c3d3567b9f22ac7
Mónica Polanco <mpolan16@g.holycross.edu>
2020-08-03 11:24:35 -0400

    added contact page

---
+++ src/Components/Pages/Contact/contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../Header';
+import Contact from '../../Contact';
+import '../../../Style';
+
+function Contact() {
+    return (
+        <div>
+            <Header value='Contact' />
+            <Contact />
+        </div>
+    );
+}
+
+export default Contact;
