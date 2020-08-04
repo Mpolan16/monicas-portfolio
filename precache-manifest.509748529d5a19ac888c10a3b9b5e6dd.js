1ac7d5487f53454e0a7230789a9d10a84dc0e95c
Mónica Polanco <mpolan16@g.holycross.edu>
2020-08-04 14:43:39 -0400

    Updates

---
+++ precache-manifest.509748529d5a19ac888c10a3b9b5e6dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8688f8b37380ee5068b70e264ccd879",
+    "url": "/monicas-portfolio/index.html"
+  },
+  {
+    "revision": "66f39882f551e8175e6e",
+    "url": "/monicas-portfolio/static/css/main.4db8bf50.chunk.css"
+  },
+  {
+    "revision": "e977637b07f43bf606cc",
+    "url": "/monicas-portfolio/static/js/2.19bae4ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monicas-portfolio/static/js/2.19bae4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f39882f551e8175e6e",
+    "url": "/monicas-portfolio/static/js/main.17723323.chunk.js"
+  },
+  {
+    "revision": "7a91f2fe29567ea99e91",
+    "url": "/monicas-portfolio/static/js/runtime-main.b79404c4.js"
+  }
+]);
