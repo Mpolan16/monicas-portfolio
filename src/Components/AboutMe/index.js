4318e0ea12b2c135f1c8d34c9d0de57b6b3a8f6f
Mónica Polanco <mpolan16@g.holycross.edu>
2020-08-04 14:25:54 -0400

    removed unecessary json file

--- src/Components/AboutMe/index.js
+++ src/Components/AboutMe/index.js
@@ -1,23 +1,16 @@
 import React from "react";
-import aboutMe from '../..//utils/aboutMe.json';
 
 function AboutMe() {
   return (
-      aboutMe.map((aboutMe,i)=>{
-        return(
-          <div Key={i}>
             <div>
-              <h4 id="Header">{aboutMe.Title}</h4>
+              <h4 id="Header">About Me</h4>
               <br/>
-              <img src={aboutMe.Image} alt={aboutMe.Alt} id="headshot" width="400"/>
+              <img src="./Assets/Headshot.png" alt="Monica Polanco" id="headshot" width="400"/>
               <br/><br/>
               <p id="abouttext">
-                  {aboutMe.Desc}
+              I am a Full-Stack Web Developer with a BA from the College of the Holy Cross. I recently graduated from UNC Chapel Hill with a certificate in Web Development and developed skills in JavaScript, HTML, CSS, jQuery, Web APIs, Node.js, MySQL, Mongo, React.js, Bootstrap, Git, Express, and Jest, to develop MERN apps serving a variety of platforms. Most recently, I worked with a team of four to develop The Tutor Tracker, a student management system web application designed for educators to organize one on one meetings with their students on an interactive calendar. Known as a team player who is passionate about creating innovative web apps, I am excited to apply what I’ve learned to new opportunities in web development. I love hanging out with my cat, eating spicy food, and learning new languages. I am open to remote opportunites.
               </p>
           </div>
-          </div>
-        )
-      })
   );
 }
 
